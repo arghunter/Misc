c4d4b0db9378094816eacfa247a7b9ff76964db3
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-06-29 10:50:18 -0700

    Create Controller.ino

---
+++ Gerald-Self-Balancer/Controller.ino
@@ -0,0 +1,150 @@
+#include "Adafruit_BNO08x_RVC.h"
+ #include <PID_v1.h>
+ Adafruit_BNO08x_RVC rvc = Adafruit_BNO08x_RVC();
+#define speedPinR 9    //  RIGHT PWM pin connect MODEL-X ENA
+#define RightMotorDirPin1  12    //Right Motor direction pin 1 to MODEL-X IN1 
+#define RightMotorDirPin2  11    //Right Motor direction pin 2 to MODEL-X IN2
+#define speedPinL 6    // Left PWM pin connect MODEL-X ENB
+#define LeftMotorDirPin1  7    //Left Motor direction pin 1 to MODEL-X IN3 
+#define LeftMotorDirPin2  8   //Left Motor direction pin 1 to MODEL-X IN4 
+
+
+#define PIN_INPUT 0
+#define PIN_OUTPUT 3
+
+int speed=100;
+double Setpoint, Input, Output;
+// double Kp=9, Ki=0.2, Kd=1.2;
+double Kp=1, Ki=0, Kd=0.;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+bool forward=true;
+/*motor control*/
+double offset=-1000;
+void go_Advance(void)  //Forward
+{
+  if( speed>=0){
+  digitalWrite(RightMotorDirPin1, HIGH);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,HIGH);
+  digitalWrite(LeftMotorDirPin2,LOW);
+  }else{
+    digitalWrite(RightMotorDirPin1, LOW);
+    digitalWrite(RightMotorDirPin2,HIGH);
+    digitalWrite(LeftMotorDirPin1,LOW);
+    digitalWrite(LeftMotorDirPin2,HIGH);
+  }
+  analogWrite(speedPinL,abs(speed));
+  analogWrite(speedPinR,abs(speed));
+}
+void go_Left(int t=0)  //Turn left
+{
+  digitalWrite(RightMotorDirPin1, HIGH);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,LOW);
+  digitalWrite(LeftMotorDirPin2,HIGH);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(t);
+}
+void go_Right(int t=0)  //Turn right
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,HIGH);
+  digitalWrite(LeftMotorDirPin1,HIGH);
+  digitalWrite(LeftMotorDirPin2,LOW);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(t);
+}
+void go_Back(int t=0)  //Reverse
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,HIGH);
+  digitalWrite(LeftMotorDirPin1,LOW);
+  digitalWrite(LeftMotorDirPin2,HIGH);
+  analogWrite(speedPinL,200);
+  analogWrite(speedPinR,200);
+  delay(t);
+}
+void stop_Stop()    //Stop
+{
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,LOW);
+  digitalWrite(LeftMotorDirPin1,LOW);
+  digitalWrite(LeftMotorDirPin2,LOW);
+}
+/*set motor speed */
+void set_Motorspeed(int speed_L,int speed_R)
+{
+  analogWrite(speedPinL,speed_L); 
+  analogWrite(speedPinR,speed_R);   
+}
+
+//Pins initialize
+void init_GPIO()
+{
+	pinMode(RightMotorDirPin1, OUTPUT); 
+	pinMode(RightMotorDirPin2, OUTPUT); 
+	pinMode(speedPinL, OUTPUT);  
+ 
+	pinMode(LeftMotorDirPin1, OUTPUT);
+  pinMode(LeftMotorDirPin2, OUTPUT); 
+  pinMode(speedPinR, OUTPUT); 
+	stop_Stop();
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);
+
+  Serial.println("Adafruit BNO08x IMU - UART-RVC mode");
+
+  Serial1.begin(115200); // This is the baud rate specified by the datasheet
+  while (!Serial1)
+    delay(10);
+
+  if (!rvc.begin(&Serial1)) { // connect to the sensor over hardware serial
+    Serial.println("Could not find BNO08x!");
+    while (1)
+      delay(10);
+  }
+
+  Serial.println("BNO08x found!");
+	init_GPIO();
+  Input = 0;
+  Setpoint = 0;
+
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-255,255);
+  
+
+  
+  // go_Back();//Reverse
+ 
+  // delay(2000);
+  
+
+  
+ 
+
+}
+
+void loop(){
+  BNO08x_RVC_Data heading;
+
+  if (!rvc.read(&heading)) {
+    return;
+  }
+
+  Input=heading.roll;
+  Input=Input;
+
+  myPID.Compute();
+  speed=Output;
+  Serial.println(speed);
+  
+  go_Advance();
+}
