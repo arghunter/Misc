c2754b61cbe345254538dfd1fdc42c2271c9aaa2
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-06-29 16:48:15 -0700

    Create driver.ino

---
+++ Optical-Flow-Mouse/driver.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(0, 1); // RX, TX
+
+void setup() {
+  // Start the built-in serial communication to the computer
+  Serial.begin(9600);
+  
+  // Start the software serial communication with the sensor
+  mySerial.begin(19200);
+}
+
+void loop() {
+  Serial.println();
+  if (mySerial.available()) {
+    // Read data from the sensor and print it to the Serial Monitor
+    while (mySerial.available()) {
+      char c = mySerial.read();
+      Serial.print("hello");
+    }
+  }
+}
