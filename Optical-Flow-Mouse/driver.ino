c7f92f02293f02d15a6d3d65d5b1234143832b69
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-06-29 18:05:42 -0700

    Update driver.ino

--- Optical-Flow-Mouse/driver.ino
+++ Optical-Flow-Mouse/driver.ino
@@ -1,21 +1,34 @@
 #include <SoftwareSerial.h>
+
 SoftwareSerial mySerial(0, 1); // RX, TX
 
 void setup() {
-  // Start the built-in serial communication to the computer
   Serial.begin(9600);
-  
-  // Start the software serial communication with the sensor
   mySerial.begin(19200);
+  delay(100);
 }
 
 void loop() {
-  // Serial.println("hellooh");
-  if (mySerial.available()) {
-    // Read data from the sensor and print it to the Serial Monitor
-    while (mySerial.available()) {
-      char c = mySerial.read();
-      Serial.println((int)c);
+  if (mySerial.available() >= 9) {
+    if (mySerial.read() == 0xFE && mySerial.read() == 0x04) {
+      uint8_t data0 = mySerial.read();
+      uint8_t data1 = mySerial.read();
+      uint8_t data2 = mySerial.read();
+      uint8_t data3 = mySerial.read();
+      uint8_t sum = mySerial.read();
+      uint8_t groundQuality = mySerial.read();
+      uint8_t packetEnd = mySerial.read();
+
+      if (packetEnd == 0xAA) {
+        if (sum == (data0 + data1 + data2 + data3)) {
+          int16_t flow_x = (int16_t)((data1 << 8) | data0);
+          int16_t flow_y = (int16_t)((data3 << 8) | data2);
+          
+          // Serial.print(flow_x);
+          // Serial.print(",");
+          Serial.println(flow_x+","+flow_y);
+        }
+      }
     }
   }
 }
